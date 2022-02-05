9ebf1ad9597b89628225c34cc9548eec10a186b9
allan <allanrey.macasaquit@gmail.com>
2022-02-05 15:44:37 -0500

    Created new Boilerplate

---
+++ dotnet_react/Domain/Activity.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public class Activity
+    {
+        public Guid ID { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public DateTime Date { get; set; }
+        public string Description { get; set; } = string.Empty;
+        public string Category { get; set; } = string.Empty;
+        public string Venue { get; set; } = string.Empty;
+
+    }
+}
